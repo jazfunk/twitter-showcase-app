264c40e8362e2210c5c67809f46180e3ee7ed1b6
Jeff King <jazfunk@gmail.com>
2020-06-25 12:30:42 -0400

    Added Empty TwitterShowcase class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import TwitterShowcase from "./TwitterShowcase"
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />        
+        <img src={logo} className="App-logo" alt="logo" />   
+        <TwitterShowcase />     
       </header>
     </div>
   );
---
+++ src/TwitterShowcase.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class TwitterShowcase extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Twitter Showcase App</h1>
+      </div>
+    )
+  }
+}
+
+export default TwitterShowcase
