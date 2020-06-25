b12b5d7d093135332baf01066fc4cece2ee37f8c
Jeff King <jazfunk@gmail.com>
2020-06-25 13:57:31 -0400

    Added Bootstrap, changed color palette.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppHeader from "./AppHeader"
 import AppMain from "./AppMain"
 import AppFooter from "./AppFooter"
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 const App = () => {
