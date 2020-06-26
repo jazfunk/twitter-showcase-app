2f1a9b2c69fc88ab14ad138bbf41ee1b4eb699a6
Jeff King <jazfunk@gmail.com>
2020-06-25 22:10:05 -0400

    Added Images and NavBar

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -4,7 +4,7 @@ import AppNav from "./AppNav"
 class AppHeader extends Component {
   render() {
     return (
-      <header className="App-header">
+      <header>
         <AppNav />
       </header>
     )
--- src/AppNav.js
+++ src/AppNav.js
@@ -1,17 +1,36 @@
-import React from "react"
+import React from "react";
+import TwitterLogo from "./images/Twitter_Logo_White.svg"
 
 const AppNav = () => {
-
   return (
-    <nav>
-      <ul className="App-nav">
-        <li>Home</li>
-        <li>Showcase</li>
-        <li>Random</li>
-      </ul>
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <img id="twitter-logo" src={TwitterLogo} alt="Twitter"></img>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" href="/">
+            Home
+          </a>
+          <a className="nav-item nav-link" href="/">
+            Showcase
+          </a>
+          <a className="nav-item nav-link" href="/">
+            Random
+          </a>
+        </div>
+      </div>
     </nav>
+  );
+};
 
-  )
-}
-
-export default AppNav
+export default AppNav;
