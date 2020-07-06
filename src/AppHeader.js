a853fb5d4aa7732c83d9e4ebdb1f4c2f4afec468
Jeff King <jazfunk@gmail.com>
2020-07-06 15:10:40 -0400

    server-work branch, adding Twitter api requests

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AppNavigation from "./AppNavigation";
+import axios from "axios";
 
 import Home from "./components/Home";
 import Showcase from "./components/Showcase";
 import Random from "./components/Random";
 
 class AppHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchURL: "",
+      authBearer:
+        "Bearer AAAAAAAAAAAAAAAAAAAAAF0IFwEAAAAAQCBmSM9wtuLGy2FVYeNr3Qo9kU4%3D2j6LiLWo1pMFZmNH9jzb0NmpaN0eDCVmCB11Gok2AakHGLt2ul",
+      }
+    }
+  
+  componentDidMount = () => {
+    var getRequest = {
+      method: 'get',
+      url: 'https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular',
+      headers: { 
+        'Content-Type': 'application/json', 
+        'Accept': 'application/json', 
+        'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAF0IFwEAAAAAQCBmSM9wtuLGy2FVYeNr3Qo9kU4%3D2j6LiLWo1pMFZmNH9jzb0NmpaN0eDCVmCB11Gok2AakHGLt2ul',
+      }
+    };
+    
+    axios(getRequest)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  
   render() {
     return (
       <BrowserRouter>
@@ -18,9 +48,9 @@ class AppHeader extends Component {
             <Route path="/random" exact component={Random} />
           </Switch>
         </header>
-      </BrowserRouter>
-    );
+      </BrowserRouter>)
   }
 }
+
 
 export default AppHeader;
